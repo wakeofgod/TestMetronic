ff6a0e2d1bfbc5e0470aea52ad29b6aaeef2b40a
wakeofgod <jltxwd@163.com>
2018-08-28 16:48:04 +0800

    资产添加5个字段

--- TestMetronic/A2ZModel/LM_T_CA_Assets.cs
+++ TestMetronic/A2ZModel/LM_T_CA_Assets.cs
@@ -9,30 +9,54 @@ namespace TestMetronic
     {
         [Display(Name = "名称"), MaxLength(40), Required]
         public string Name { get; set; }
+
         [Display(Name = "公司"), Required]
         public int CompanyID { get; set; }
-        //设备照片,可以为空
-        public byte[] AssetInfo { get; set; }
-        //照片格式
-        public string InfoType { get; set; }
-        //物理地址
-        public string InfoUrl { get; set; }
 
-        [Display(Name = "描述"), MaxLength(500), Required]
-        public string  Description { get; set; }
-        [Display(Name = "所有者"), MaxLength(500), Required]
+        [Display(Name = "创建人"), Required]
+        public Guid Creator { get; set; }
+
+        public byte[] Icon { get; set; }
+
+        public string FileExtName { get; set; }
+
+        public string FileName { get; set; }
+
+        [Display(Name = "描述"), MaxLength(500)]
+        public string Description { get; set; }
+
+        [Display(Name = "所有者"), Required]
         public Guid AssetOwner { get; set; }
-        [Display(Name = "制造商"), Required]
-        public Guid Manufacturer { get; set; }
+
+        [Display(Name = "制造商")]
+        public Guid? Manufacturer { get; set; }
+
         [Display(Name = "当前位置"), Required]
         public Guid CurrentLocation { get; set; }
+
         [Display(Name = "存放位置"), Required]
         public Guid StorageLocation { get; set; }
-        [Display(Name = "工具类型"), Required]
-        public Guid Type { get; set; }
-        [Display(Name = "状态"), Required]
-        public int Status { get; set; }
 
-        public DateTime DateAcquired { get; set; }
+        [Display(Name = "工具类型")]
+        public Guid? Type { get; set; }
+
+        [Display(Name = "状态"), Required]
+        public Guid Status { get; set; }
+
+        [Display(Name = "状态1")]
+        public Guid? Status1 { get; set; }
+
+        public DateTime? DateAcquired { get; set; }
+
+        public int OrderIndex { get; set; }
+
+        //2018/08/28 新加5个字段
+        public Guid? Sheet { get; set; }
+        [MaxLength(40)]
+        public string Units { get; set; }
+
+        public float? Resolution { get; set; }
+        public float? Range1 { get; set; }
+        public float? Range2 { get; set; }
     }
 }
